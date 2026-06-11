345e06577d7697f3ec1ccd176e3b3262aadb8810
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-11 16:34:24 +0545

    added two numbers

---
+++ AddTwonum.py
@@ -0,0 +1,19 @@
+# With predefined variables 
+num1= 30
+num2 = 40
+sum = num1 + num2
+
+#print("The sum of the given numbers are :",num1 + num2)
+
+print("The sum the given numbers are:",sum)
+
+
+# with user inputs
+
+num1 = float(input("Enter the first nunnber:"))
+
+num2 = float(input("Enter the second number:"))
+
+sum = num1 + num2
+
+print(f"The sum of the given numbers are:",sum)
