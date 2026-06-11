f3146a3672540048bff6cfbf8edf2112dfb1b09e
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-11 16:57:56 +0545

    Swapping two variable

---
+++ SwapVar.py
@@ -0,0 +1,23 @@
+# Using third variable
+
+x = 13
+y = 12
+
+temp = x
+
+print("The value of temp variable is",temp)
+
+x=y
+print("The value of x is",x)
+
+y = temp
+print("The value of y is:",y)
+
+print("The value of x and y is :",x,y)
+# Without using third variable
+
+x , y = y , x
+
+print("The value of x is :",x)
+
+print("The value of y is:",y)
