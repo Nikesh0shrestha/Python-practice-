8af6353a6ec4c3147314e44f96bbafeac623d262
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-11 16:44:11 +0545

    SquareRoot of two nunbers using exponentiational and math module

---
+++ SquareRoot.py
@@ -0,0 +1,20 @@
+#using Exponentioals 
+
+# num = 64
+num1 = int(input("Enter a number here:"))
+
+sr = num1**(1/2)
+
+print("The square root of the  given number is:",sr)
+
+
+# Using math module 
+import math
+ 
+num = int(input("Enter a number:"))
+
+sr = math.sqrt(num)
+
+print("The Square root of the given number is :", sr)
+
+
