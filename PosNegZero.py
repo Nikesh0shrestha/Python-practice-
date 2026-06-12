e7e258ebfcfbc49e2dd270f75eb5b52c2500fce8
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-12 16:31:14 +0545

    Check the given number is positive, negative or zero

---
+++ PosNegZero.py
@@ -0,0 +1,11 @@
+# Program to check to check if a number is positive, negative or zero
+
+# num = 12
+num = int(input("Enter a number:"))
+
+if num > 0:
+    print("The given number is positive.")
+elif num == 0:
+    print("The given number is zero.")
+else:
+    print("The given number is negative.")
