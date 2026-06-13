3c7a4e3201d87676d79e3690a0f7438d52040da7
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-13 12:54:42 +0545

    converted celcius to fahrenheit

---
+++ celciusTofahrenheit .py
@@ -0,0 +1,7 @@
+#  Program to convert to celcius to fahrenheit
+
+celcius = int(input("Enter the temperature in Celsius:"))
+
+fahrenheit = (celcius*(9/5)) +32
+
+print("The converted value is ", fahrenheit,"Fahrenheit")
